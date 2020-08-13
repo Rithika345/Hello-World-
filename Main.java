45ea966201418cedd81d93be959949761fe00f86
Rithika345 <69615436+Rithika345@users.noreply.github.com>
2020-08-12 20:50:46 -0700

    Add files via upload
    Hello World Project

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hi! My name is Rithika");
+		System.out.println("I do speech and debate");
+		System.out.println("I like to draw");
+		System.out.println("I lived in 3 continents");
+	}
+}
